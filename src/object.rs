14f8e98eda8e3f621f560f07f0a20c8c8f1b9bd8
Amirhossein Ghanipour <d3v1ll3n@gmail.com>
2025-08-29 07:27:51 +0330

    refactor ObjectId and PyGCHead structures for improved clarity and performance, updating field access and default implementations, and enhancing PyObject management with new methods for reference counting and finalization

--- src/object.rs
+++ src/object.rs
@@ -1,19 +1,9 @@
-use std::any::Any;
-use std::sync::atomic::{AtomicUsize, Ordering};
-use std::sync::{Arc, RwLock};
+use std::ffi::c_void;
+use std::hash::{Hash, Hasher};
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-pub struct ObjectId(usize);
-
-impl ObjectId {
-    pub fn new() -> Self {
-        static COUNTER: AtomicUsize = AtomicUsize::new(1);
-        ObjectId(COUNTER.fetch_add(1, Ordering::Relaxed))
-    }
-
-    pub fn as_usize(&self) -> usize {
-        self.0
-    }
+pub struct ObjectId {
+    pub id: usize,
 }
 
 impl Default for ObjectId {
@@ -22,64 +12,25 @@ impl Default for ObjectId {
     }
 }
 
-#[derive(Debug, Clone)]
-pub struct PyGCHead {
-    pub _gc_next: usize,
-
-    pub _gc_prev: usize,
-
-    gc_refs: isize,
-
-    collecting: bool,
-
-    finalized: bool,
-}
-
-impl PyGCHead {
+impl ObjectId {
     pub fn new() -> Self {
-        Self {
-            _gc_next: 0,
-            _gc_prev: 0,
-            gc_refs: 0,
-            collecting: false,
-            finalized: false,
+        static mut COUNTER: usize = 0;
+        unsafe {
+            COUNTER += 1;
+            Self { id: COUNTER }
         }
     }
 
-    pub fn get_refs(&self) -> isize {
-        self.gc_refs
+    pub fn as_usize(&self) -> usize {
+        self.id
     }
+}
 
-    pub fn set_refs(&mut self, refs: isize) {
-        self.gc_refs = refs;
-    }
-
-    pub fn set_collecting(&mut self) {
-        self.collecting = true;
-        self._gc_prev |= 2;
-    }
-
-    pub fn clear_collecting(&mut self) {
-        self.collecting = false;
-        self._gc_prev &= !2;
-    }
-
-    pub fn is_collecting(&self) -> bool {
-        self.collecting
-    }
-
-    pub fn set_finalized(&mut self) {
-        self.finalized = true;
-        self._gc_prev |= 1;
-    }
-
-    pub fn is_finalized(&self) -> bool {
-        self.finalized
-    }
-
-    pub fn is_tracked(&self) -> bool {
-        self._gc_next != 0
-    }
+#[repr(C)]
+#[derive(Debug, Clone)]
+pub struct PyGCHead {
+    pub _gc_next: usize,
+    pub _gc_prev: usize,
 }
 
 impl Default for PyGCHead {
@@ -88,137 +39,168 @@ impl Default for PyGCHead {
     }
 }
 
-#[derive(Debug, Clone)]
+impl PyGCHead {
+    pub fn new() -> Self {
+        Self {
+            _gc_next: 0,
+            _gc_prev: 0,
+        }
+    }
+
+    pub fn set_next(&mut self, next: *mut PyGCHead) {
+        self._gc_next = next as usize;
+    }
+
+    pub fn get_next(&self) -> *mut PyGCHead {
+        self._gc_next as *mut PyGCHead
+    }
+
+    pub fn set_prev(&mut self, prev: *mut PyGCHead) {
+        self._gc_prev = (self._gc_prev & 0x3) | (prev as usize);
+    }
+
+    pub fn get_prev(&self) -> *mut PyGCHead {
+        (self._gc_prev & !0x3) as *mut PyGCHead
+    }
+
+    pub fn set_refs(&mut self, refs: isize) {
+        self._gc_prev = (self._gc_prev & 0x3) | ((refs as usize) << 2);
+    }
+
+    pub fn get_refs(&self) -> isize {
+        ((self._gc_prev >> 2) & 0x3FFFFFFFFFFFFFFF) as isize
+    }
+
+    pub fn set_collecting(&mut self) {
+        self._gc_prev |= 0x2;
+    }
+
+    pub fn clear_collecting(&mut self) {
+        self._gc_prev &= !0x2;
+    }
+
+    pub fn is_collecting(&self) -> bool {
+        (self._gc_prev & 0x2) != 0
+    }
+
+    pub fn set_finalized(&mut self) {
+        self._gc_prev |= 0x1;
+    }
+
+    pub fn is_finalized(&self) -> bool {
+        (self._gc_prev & 0x1) != 0
+    }
+
+    pub fn set_unreachable(&mut self) {
+        self._gc_next |= 0x1;
+    }
+
+    pub fn clear_unreachable(&mut self) {
+        self._gc_next &= !0x1;
+    }
+
+    pub fn is_unreachable(&self) -> bool {
+        (self._gc_next & 0x1) != 0
+    }
+
+    pub fn is_tracked(&self) -> bool {
+        self._gc_next != 0
+    }
+}
+
+#[derive(Debug, Clone, PartialEq)]
 pub enum ObjectData {
     Integer(i64),
-
+    Float(f64),
     String(String),
-
     List(Vec<PyObject>),
-
     Dict(Vec<(PyObject, PyObject)>),
-
-    Tuple(Vec<PyObject>),
-
-    Set(Vec<PyObject>),
-
-    Custom(Arc<dyn Any + Send + Sync>),
-
+    Custom(*mut c_void),
     None,
 }
 
-#[derive(Debug, Clone)]
 #[repr(C)]
+#[derive(Debug, Clone)]
 pub struct PyObject {
-    pub id: ObjectId,
+    pub gc_head: PyGCHead,
+    pub name: String,
+    pub data: ObjectData,
+    pub refcount: usize,
     pub gc_tracked: bool,
     pub has_finalizer: bool,
-    pub refcount: Arc<AtomicUsize>,
-
-    pub gc_head: Option<PyGCHead>,
-
-    pub type_name: String,
-    pub data: Arc<RwLock<ObjectData>>,
-    pub original_ptr: Option<*mut std::ffi::c_void>,
+    pub id: ObjectId,
 }
 
 impl PyObject {
-    pub fn new(type_name: String, data: ObjectData) -> Self {
+    pub fn new(name: String, data: ObjectData) -> Self {
         Self {
-            id: ObjectId::new(),
-            type_name,
-            data: Arc::new(RwLock::new(data)),
-            refcount: Arc::new(AtomicUsize::new(1)),
+            gc_head: PyGCHead::new(),
+            name,
+            data,
+            refcount: 1,
             gc_tracked: false,
-            gc_head: None,
             has_finalizer: false,
-            original_ptr: None,
+            id: ObjectId::new(),
         }
     }
 
-    pub fn new_ffi(type_name: &str, data: ObjectData, ptr: *mut std::ffi::c_void) -> Self {
+    pub fn new_ffi(name: &str, data: ObjectData, _ptr: *mut c_void) -> Self {
         Self {
-            id: ObjectId::new(),
-            type_name: type_name.to_string(),
-            data: Arc::new(RwLock::new(data)),
-            refcount: Arc::new(AtomicUsize::new(1)),
+            gc_head: PyGCHead::new(),
+            name: name.to_string(),
+            data,
+            refcount: 1,
             gc_tracked: false,
-            gc_head: None,
             has_finalizer: false,
-            original_ptr: Some(ptr),
+            id: ObjectId::new(),
         }
     }
 
-    pub fn new_with_finalizer(type_name: String, data: ObjectData) -> Self {
+    pub fn new_with_finalizer(name: String, data: ObjectData) -> Self {
         Self {
-            id: ObjectId::new(),
-            type_name,
-            data: Arc::new(RwLock::new(data)),
-            refcount: Arc::new(AtomicUsize::new(1)),
+            gc_head: PyGCHead::new(),
+            name,
+            data,
+            refcount: 1,
             gc_tracked: false,
-            gc_head: None,
             has_finalizer: true,
-            original_ptr: None,
-        }
-    }
-
-    pub fn new_with_ptr(type_name: String, data: ObjectData, ptr: *mut std::ffi::c_void) -> Self {
-        Self {
             id: ObjectId::new(),
-            type_name,
-            data: Arc::new(RwLock::new(data)),
-            refcount: Arc::new(AtomicUsize::new(1)),
-            gc_tracked: false,
-            gc_head: None,
-            has_finalizer: false,
-            original_ptr: Some(ptr),
         }
     }
 
     pub fn get_refcount(&self) -> usize {
-        self.refcount.load(Ordering::Relaxed)
+        self.refcount
     }
 
-    pub fn incref(&self) -> usize {
-        self.refcount.fetch_add(1, Ordering::Relaxed) + 1
+    pub fn set_refcount(&mut self, count: usize) {
+        self.refcount = count;
     }
 
-    pub fn decref(&self) -> usize {
-        let current = self.refcount.fetch_sub(1, Ordering::Relaxed);
-        if current == 0 {
-            panic!("Reference count went below 0");
+    pub fn inc_ref(&mut self) {
+        self.refcount += 1;
+    }
+
+    pub fn dec_ref(&mut self) -> bool {
+        if self.refcount > 0 {
+            self.refcount -= 1;
+            self.refcount == 0
+        } else {
+            false
         }
-        current - 1
     }
 
-    pub fn should_track(&self) -> bool {
-        matches!(
-            &*self.data.try_read().unwrap(),
-            ObjectData::List(_)
-                | ObjectData::Dict(_)
-                | ObjectData::Tuple(_)
-                | ObjectData::Set(_)
-                | ObjectData::Custom(_)
-        )
+    pub fn set_finalizer(&mut self, has_finalizer: bool) {
+        self.has_finalizer = has_finalizer;
     }
 
-    pub fn matches_ptr(&self, ptr: *mut std::ffi::c_void) -> bool {
-        self.original_ptr
-            .map(|orig_ptr| orig_ptr == ptr)
-            .unwrap_or(false)
+    pub fn has_finalizer(&self) -> bool {
+        self.has_finalizer
     }
+}
 
-    pub fn get_size(&self) -> usize {
-        match &*self.data.try_read().unwrap() {
-            ObjectData::Integer(_) => 8,
-            ObjectData::String(s) => s.len(),
-            ObjectData::List(l) => l.len() * std::mem::size_of::<PyObject>(),
-            ObjectData::Dict(d) => d.len() * std::mem::size_of::<(PyObject, PyObject)>(),
-            ObjectData::Tuple(t) => t.len() * std::mem::size_of::<PyObject>(),
-            ObjectData::Set(s) => s.len() * std::mem::size_of::<PyObject>(),
-            ObjectData::Custom(_) => std::mem::size_of::<Arc<dyn Any + Send + Sync>>(),
-            ObjectData::None => 0,
-        }
+impl Hash for PyObject {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.id.hash(state);
     }
 }
 
@@ -230,82 +212,63 @@ impl PartialEq for PyObject {
 
 impl Eq for PyObject {}
 
-impl std::hash::Hash for PyObject {
-    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
-        self.id.hash(state);
-    }
-}
-
-impl Default for PyObject {
-    fn default() -> Self {
-        Self::new("object".to_string(), ObjectData::None)
-    }
-}
-
 pub struct PyObjectPtr {
     ptr: *mut PyObject,
 }
 
 impl PyObjectPtr {
-    /// Creates a new `PyObjectPtr` from a raw pointer.
+    /// Create a new PyObjectPtr from a raw pointer
     ///
     /// # Safety
     ///
-    /// The caller must ensure that `ptr` is a valid pointer to a `PyObject` or `null`.
-    /// If `ptr` is not null, it must point to a valid, initialized `PyObject` instance.
+    /// - `ptr` must be a valid pointer to a PyObject
+    /// - The pointer must remain valid for the lifetime of the PyObjectPtr
+    /// - The caller is responsible for ensuring the pointer is not used after the PyObject is dropped
     pub unsafe fn new(ptr: *mut PyObject) -> Self {
         Self { ptr }
-    }
-
-    pub fn as_ptr(&self) -> *mut PyObject {
-        self.ptr
-    }
-
-    /// Returns a reference to the underlying `PyObject` if the pointer is not null.
-    ///
-    /// # Safety
-    ///
-    /// The caller must ensure that `self.ptr` points to a valid, initialized `PyObject` instance.
-    /// This function assumes the pointer is valid when not null.
-    pub unsafe fn as_ref(&self) -> Option<&PyObject> {
-        if self.ptr.is_null() {
-            None
-        } else {
-            unsafe { Some(&*self.ptr) }
-        }
-    }
-
-    /// Returns a mutable reference to the underlying `PyObject` if the pointer is not null.
-    ///
-    /// # Safety
-    ///
-    /// The caller must ensure that `self.ptr` points to a valid, initialized `PyObject` instance.
-    /// This function assumes the pointer is valid when not null.
-    pub unsafe fn as_mut(&mut self) -> Option<&mut PyObject> {
-        if self.ptr.is_null() {
-            None
-        } else {
-            unsafe { Some(&mut *self.ptr) }
-        }
     }
 
     pub fn is_null(&self) -> bool {
         self.ptr.is_null()
     }
-}
 
-impl Clone for PyObjectPtr {
-    fn clone(&self) -> Self {
-        *self
+    /// Get a reference to the PyObject
+    ///
+    /// # Safety
+    ///
+    /// - The underlying pointer must be valid and point to a PyObject
+    /// - The PyObject must not be modified while this reference exists
+    /// - The caller must ensure the PyObject remains valid for the lifetime of the reference
+    pub unsafe fn as_ref(&self) -> Option<&PyObject> {
+        if self.ptr.is_null() {
+            None
+        } else {
+            Some(unsafe { &*self.ptr })
+        }
+    }
+
+    /// Get a mutable reference to the PyObject
+    ///
+    /// # Safety
+    ///
+    /// - The underlying pointer must be valid and point to a PyObject
+    /// - The PyObject must not be accessed by other code while this mutable reference exists
+    /// - The caller must ensure the PyObject remains valid for the lifetime of the reference
+    pub unsafe fn as_mut(&mut self) -> Option<&mut PyObject> {
+        if self.ptr.is_null() {
+            None
+        } else {
+            Some(unsafe { &mut *self.ptr })
+        }
     }
 }
 
-impl Copy for PyObjectPtr {}
-
-impl Default for PyObjectPtr {
-    fn default() -> Self {
-        Self {
-            ptr: std::ptr::null_mut(),
+impl Drop for PyObjectPtr {
+    fn drop(&mut self) {
+        if !self.ptr.is_null() {
+            unsafe {
+                let _ = Box::from_raw(self.ptr);
+            }
         }
     }
 }
